c8e13239426e58375be1583b52470caf84abb2f2
lihao <942993806@qq.com>
2021-03-02 09:40:12 +0800

    提交index首页

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(function () {
+
+    //渲染用户头像
+    function getUserHead(user) {
+        //1 获取用户的名称
+        var uname = user.nickname || user.username
+        //2  设置欢迎文本
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + uname);
+        // 3 设置头像
+        if (user.user_pic !== null) {
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+            $('.textName').hide()
+        } else {
+            $('.layui-nav-img').hide()
+            var first = uname[0].toUpperCase()
+
+            $(".textName").html(first).show()
+        }
+    }
+
+    //实现点击退出功能
+    var layer = layui.layer
+    $('#goOut').on('click', function () {
+        //提示用户是否退出
+        layer.confirm('确定要退出吗?', { icon: 3, title: '提示' }, function (index) {
+            //点击确定，回到登陆页面
+            //1，清空本地存储的token
+            localStorage.removeItem('token')
+            //2 重新跳转到登陆页面
+            location.href = '/login.html'
+            //3 清除询问框
+            layer.close(index);
+        });
+    })
+    getUserInfo()
+    //获取用户基本信息
+    function getUserInfo() {
+        $.ajax({
+            type: "get",
+            url: "/my/userinfo",
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取数据失败');
+                }
+                getUserHead(res.data)
+            }
+        });
+
+    }
+})
